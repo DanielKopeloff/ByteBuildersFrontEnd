caea95ffeacb2f70cba07506cbe161815dfa6d5e
DanielKopeloff <danielkopeloff@gmail.com>
2021-04-08 19:52:14 -0400

    Adding files

---
+++ project2-frontend/src/app/sales-person-list/sales-person.spec.ts
@@ -0,0 +1,8 @@
+import { SalesPerson } from './sales-person';
+
+describe('SalesPerson', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    expect(new SalesPerson()).toBeTruthy();
+  });
+});
