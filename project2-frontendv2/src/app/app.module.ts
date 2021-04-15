26e1689ece142a9091e1ccfa87f4c408c777923f
DanRey321 <danrey321@gmail.com>
2021-04-15 14:48:15 -0700

    Dont push to main/ broken

--- project2-frontendv2/src/app/app.module.ts
+++ project2-frontendv2/src/app/app.module.ts
@@ -7,21 +7,52 @@ import {HttpClientModule} from '@angular/common/http';
 import {ProductService} from './services/product.service';
 import {ByteUserService} from './services/byte-user.service';
 import { ByteUserListComponent } from './components/byte-user-list/byte-user-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent
+} from '@okta/okta-angular';
+
+import myAppConfig from './config/my-app-config';
+
+import {Router, Routes} from '@angular/router';
+
+const oktaConfig = Object.assign({
+  onAuthRequired: (injector) => {
+    const router = injector.get(Router);
+
+    router.navigate(['/login']);
+  }
+}, myAppConfig.oidc);
+
+const routes: Routes = [
+// tslint:disable-next-line:no-unused-expression
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ByteUserListComponent
+    ByteUserListComponent,
+    LoginComponent,
+    LoginStatusComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    OktaAuthModule
   ],
   providers: [
     ProductService,
     ByteUserService,
-  ],
-  bootstrap: [AppComponent]
+    {provide: OKTA_CONFIG, useValue: oktaConfig}
+  ]
+  // providers: [ProductService, {provide: OKTA_CONFIG, useValue: oktaConfig}],
+  // bootstrap: [AppComponent]
 })
 export class AppModule { }
