56ebcb87aeb7be695c1ae462db08367cd67bbe29
Daniel Reyes <35903470+DanRey321@users.noreply.github.com>
2021-04-19 08:02:13 -0700

    Login/Register

--- project2-frontendv2/src/app/components/login/login.component.ts
+++ project2-frontendv2/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ByteUserService} from '../../services/byte-user.service';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  LoginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  error: string;
+
+  // tslint:disable-next-line:no-shadowed-variable
+  constructor(private formBuilder: FormBuilder, private ByteUserService: ByteUserService) {
+    // formBuilder.createSection('#myForm', myData);
+
+  }
 
   ngOnInit(): void {
+    this.LoginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  // tslint:disable-next-line:typedef
+  onSubmit(){
+    this.submitted = true;
+    console.log(this.LoginForm.value);
+    if (this.LoginForm.invalid){return; }
+
+    this.loading = true;
+    this.ByteUserService.LoginUser(this.LoginForm.value);
   }
 
 }
--- project2-frontendv2/src/app/components/register/register.component.ts
+++ project2-frontendv2/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ByteUserService} from '../../services/byte-user.service';
+import {first} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  Register: FormGroup;
+  loading = false;
+  submitted = false;
+  error: string;
+
+  // tslint:disable-next-line:no-shadowed-variable
+  constructor(private formBuilder: FormBuilder, private ByteUserService: ByteUserService) { }
 
   ngOnInit(): void {
+
+    this.Register = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', Validators.required],
+      role: ['1', Validators.required],
+    });
   }
+
+  // tslint:disable-next-line:typedef
+  get f() { return this.Register.controls; }
+
+  // tslint:disable-next-line:typedef
+  onSubmit(){
+    this.submitted = true;
+    console.log(this.Register.value);
+    if (this.Register.invalid){return; }
+
+    // this.loading = true;
+    this.ByteUserService.registerUser(this.Register.value);
+      }
 
 }
